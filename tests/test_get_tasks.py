29ef4224d402c6d275baae0086765449218feaac
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2021-07-16 13:06:48 +0300

    Мерж тестов

--- tests/test_get_tasks.py
+++ tests/test_get_tasks.py
@@ -9,17 +9,9 @@ from test_initialization import client, database
 @pytest.mark.usefixtures(client.__name__)
 @pytest.mark.usefixtures(database.__name__)
 async def test_get_variants(client: AsyncClient):
-    response = await client.get("/variants/")
-    assert response.status_code == 200
-    models = [VariantReducedModel.parse_obj(entry) for entry in response.json()]
-    assert len(models) == 2
-
-@pytest.mark.asyncio
-@pytest.mark.usefixtures(client.__name__)
-@pytest.mark.usefixtures(database.__name__)
-async def test_get_variants(client: AsyncClient):
     response_variants = await client.get("/variants/")
     assert response_variants.status_code == 200
+    assert len(response_variants.json()) == 2
     for variant in response_variants.json():
         response_variant = await client.get("/variants/%s" % variant["id"])
         assert response_variant.status_code == 200
